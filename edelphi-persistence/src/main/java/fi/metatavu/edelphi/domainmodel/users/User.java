9775898e3438d2025fa2ac703bd3e0cc83f72638
Antti Leppä <antti.leppa@metatavu.fi>
2016-09-27 06:43:17 +0300

    Fixed SonarQube issues on User entity

--- edelphi-persistence/src/main/java/fi/metatavu/edelphi/domainmodel/users/User.java
+++ edelphi-persistence/src/main/java/fi/metatavu/edelphi/domainmodel/users/User.java
@@ -41,6 +41,52 @@ import fi.metatavu.edelphi.domainmodel.base.ModificationTrackedEntity;
 @Inheritance(strategy=InheritanceType.JOINED)
 public class User implements ArchivableEntity, ModificationTrackedEntity {
 
+  @Id
+  @DocumentId
+  @GeneratedValue(strategy=GenerationType.TABLE, generator="User")  
+  @TableGenerator(name="User", initialValue=1, allocationSize=100, table = "hibernate_sequences", pkColumnName = "sequence_name", valueColumnName = "sequence_next_hi_value")
+  private Long id;
+  
+  @Field(index = Index.YES, store = Store.NO)
+  private String firstName;
+
+  @Field(index = Index.YES, store = Store.NO)
+  private String lastName;
+  
+  private String nickname;
+  
+  @ManyToOne
+  private UserEmail defaultEmail;
+
+  @OneToMany (fetch = FetchType.LAZY)
+  @JoinColumn (name="user_id")
+  @IndexedEmbedded
+  private List<UserEmail> emails = new ArrayList<>();
+
+  @NotNull
+  @Column(nullable = false)
+  @Field(index = Index.YES, store = Store.NO)
+  private Boolean archived = Boolean.FALSE;
+
+  @ManyToOne 
+  private User creator;
+  
+  @NotNull
+  @Column (updatable=false, nullable=false)
+  @Temporal (value=TemporalType.TIMESTAMP)
+  private Date created;
+  
+  @ManyToOne  
+  private User lastModifier;
+  
+  @NotNull
+  @Column (nullable=false)
+  @Temporal (value=TemporalType.TIMESTAMP)
+  private Date lastModified;
+
+  @Temporal (value=TemporalType.TIMESTAMP)
+  private Date lastLogin;
+  
   /**
    * Returns internal unique id
    * 
@@ -82,34 +128,42 @@ public class User implements ArchivableEntity, ModificationTrackedEntity {
     return defaultEmail;
   }
 
+  @Override
   public void setCreator(User creator) {
     this.creator = creator;
   }
 
+  @Override
   public User getCreator() {
     return creator;
   }
 
+  @Override
   public void setCreated(Date created) {
     this.created = created;
   }
 
+  @Override
   public Date getCreated() {
     return created;
   }
 
+  @Override
   public void setLastModifier(User lastModifier) {
     this.lastModifier = lastModifier;
   }
 
+  @Override
   public User getLastModifier() {
     return lastModifier;
   }
 
+  @Override
   public void setLastModified(Date lastModified) {
     this.lastModified = lastModified;
   }
 
+  @Override
   public Date getLastModified() {
     return lastModified;
   }
@@ -122,10 +176,12 @@ public class User implements ArchivableEntity, ModificationTrackedEntity {
     return lastLogin;
   }
 
+  @Override
   public void setArchived(Boolean archived) {
     this.archived = archived;
   }
 
+  @Override
   public Boolean getArchived() {
     return archived;
   }
@@ -142,10 +198,26 @@ public class User implements ArchivableEntity, ModificationTrackedEntity {
 
   @Transient
   public String getFullName(boolean lastNameFirst, boolean useMailIfNull) {
-    String fn = getFirstName();
-    String ln = getLastName();
-    String name = fn == null ? ln == null ? null : ln : ln == null ? fn : lastNameFirst ? ln + ", " + fn : fn + " " + ln;
-    return name == null && useMailIfNull ? getDefaultEmailAsString() : name; 
+    boolean firstNameBlank = StringUtils.isBlank(firstName);
+    boolean lastNameBlank = StringUtils.isBlank(lastName);
+        
+    if (firstNameBlank && lastNameBlank) {
+      return useMailIfNull ? getDefaultEmailAsString() : null;
+    } else {
+      return printFullName(lastNameFirst, firstNameBlank, lastNameBlank);
+    }
+  }
+
+  private String printFullName(boolean lastNameFirst, boolean firstNameBlank, boolean lastNameBlank) {
+    if (!firstNameBlank && !lastNameBlank) {
+      return lastNameFirst ? String.format("%s, %s", lastName, firstName) : String.format("%s %s", firstName, lastName);
+    }
+    
+    if (!firstNameBlank) {
+      return firstName;
+    }
+    
+    return lastName;
   }
   
   @Transient
@@ -193,51 +265,5 @@ public class User implements ArchivableEntity, ModificationTrackedEntity {
   public List<UserEmail> getEmails() {
     return emails;
   }
-
-  @Id
-  @DocumentId
-  @GeneratedValue(strategy=GenerationType.TABLE, generator="User")  
-  @TableGenerator(name="User", initialValue=1, allocationSize=100, table = "hibernate_sequences", pkColumnName = "sequence_name", valueColumnName = "sequence_next_hi_value")
-  private Long id;
-  
-  @Field(index = Index.YES, store = Store.NO)
-  private String firstName;
-
-  @Field(index = Index.YES, store = Store.NO)
-  private String lastName;
-  
-  private String nickname;
-  
-  @ManyToOne
-  private UserEmail defaultEmail;
-
-  @OneToMany (fetch = FetchType.LAZY)
-  @JoinColumn (name="user_id")
-  @IndexedEmbedded
-  private List<UserEmail> emails = new ArrayList<UserEmail>();
-
-  @NotNull
-  @Column(nullable = false)
-  @Field(index = Index.YES, store = Store.NO)
-  private Boolean archived = Boolean.FALSE;
-
-  @ManyToOne 
-  private User creator;
-  
-  @NotNull
-  @Column (updatable=false, nullable=false)
-  @Temporal (value=TemporalType.TIMESTAMP)
-  private Date created;
-  
-  @ManyToOne  
-  private User lastModifier;
-  
-  @NotNull
-  @Column (nullable=false)
-  @Temporal (value=TemporalType.TIMESTAMP)
-  private Date lastModified;
-
-  @Temporal (value=TemporalType.TIMESTAMP)
-  private Date lastLogin;
 
 }
