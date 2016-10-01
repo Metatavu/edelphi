f84a908ea98c1f79eec352825fda19f0a457b465
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-01 12:09:19 +0300

    Fixed sonar issues on InternalAuthenticationStrategy

--- edelphi/src/main/java/fi/metatavu/edelphi/auth/InternalAuthenticationStrategy.java
+++ edelphi/src/main/java/fi/metatavu/edelphi/auth/InternalAuthenticationStrategy.java
@@ -17,19 +17,21 @@ import fi.metatavu.edelphi.domainmodel.users.UserPassword;
 import fi.metatavu.edelphi.i18n.Messages;
 import fi.metatavu.edelphi.utils.RequestUtils;
 
-public class InternalAuthenticationStrategy 
-    extends AbstractAuthenticationStrategy 
-    implements AuthenticationProvider {
-  
+/**
+ * Strategy that uses email and password for user authentication
+ * 
+ * @author Antti Leppä
+ */
+public class InternalAuthenticationStrategy extends AbstractAuthenticationStrategy implements AuthenticationProvider {
+
   public static final String STRATEGY_NAME = "eDelfoi";
-  
-  public InternalAuthenticationStrategy() {
-  }
-  
+
+  @Override
   public String getName() {
     return STRATEGY_NAME;
   }
-  
+
+  @Override
   public boolean requiresCredentials() {
     return true;
   }
@@ -41,41 +43,44 @@ public class InternalAuthenticationStrategy
 
     String username = StringUtils.lowerCase(requestContext.getString("username"));
     String password = RequestUtils.md5EncodeString(requestContext.getString("password"));
-    
+
     UserEmailDAO userEmailDAO = new UserEmailDAO();
     UserPasswordDAO userPasswordDAO = new UserPasswordDAO();
 
     UserEmail userEmail = userEmailDAO.findByAddress(username);
-    
+
     if (userEmail != null) {
       User user = userEmail.getUser();
       UserPassword userPassword = userPasswordDAO.findByUser(user);
       UserActivationDAO userActivationDAO = new UserActivationDAO();
       UserActivation userActivation = userActivationDAO.findByUser(user);
-      
+
       if (userActivation != null) {
-        String errorLink = messages.getText(locale,  "exception.1039.accountNotYetActivated.link");
-        errorLink = "<a href=\"" + RequestUtils.getBaseUrl(requestContext.getRequest()) + "/resendactivation.page?email=" + username + "\">" + errorLink + "</a>";
-        String errorTemplate = messages.getText(locale,  "exception.1039.accountNotYetActivated.template", new String[] { errorLink });
+        String errorLink = messages.getText(locale, "exception.1039.accountNotYetActivated.link");
+        errorLink = "<a href=\"" + RequestUtils.getBaseUrl(requestContext.getRequest())
+            + "/resendactivation.page?email=" + username + "\">" + errorLink + "</a>";
+        String errorTemplate = messages.getText(locale, "exception.1039.accountNotYetActivated.template",
+            new String[] { errorLink });
         throw new SmvcRuntimeException(EdelfoiStatusCode.ACCOUNT_NOT_YET_ACTIVATED, errorTemplate);
       }
-      
+
       if (userPassword != null && password.equals(userPassword.getPasswordHash())) {
         RequestUtils.loginUser(requestContext, user);
         return AuthenticationResult.LOGIN;
-      }
-      else {
-        throw new SmvcRuntimeException(EdelfoiStatusCode.INVALID_LOGIN, messages.getText(locale, "exception.1007.invalidLogin"));
+      } else {
+        throw new SmvcRuntimeException(EdelfoiStatusCode.INVALID_LOGIN,
+            messages.getText(locale, "exception.1007.invalidLogin"));
       }
     } else {
-      throw new SmvcRuntimeException(EdelfoiStatusCode.INVALID_LOGIN, messages.getText(locale, "exception.1007.invalidLogin"));
+      throw new SmvcRuntimeException(EdelfoiStatusCode.INVALID_LOGIN,
+          messages.getText(locale, "exception.1007.invalidLogin"));
     }
   }
 
   @Override
   public String[] getKeys() {
     // Internal login needs no settings
-    return null;
+    return new String[0];
   }
 
   @Override
