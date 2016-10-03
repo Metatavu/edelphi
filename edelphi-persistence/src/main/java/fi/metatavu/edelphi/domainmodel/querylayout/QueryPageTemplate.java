f51eeff936b17c0738795bddd28958553c937038
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-03 21:01:29 +0300

    Fixed issue on QueryPageTemplate that prevented loading of entities completely

--- edelphi-persistence/src/main/java/fi/metatavu/edelphi/domainmodel/querylayout/QueryPageTemplate.java
+++ edelphi-persistence/src/main/java/fi/metatavu/edelphi/domainmodel/querylayout/QueryPageTemplate.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -114,6 +116,7 @@ public class QueryPageTemplate implements ArchivableEntity, ModificationTrackedE
 
   @NotNull
   @Column(nullable = false)
+  @Enumerated (EnumType.STRING)
   private QueryPageType pageType;
   
   @NotNull
