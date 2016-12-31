3c1867943292db69b4e49d804dd1efa7be707420
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-31 08:44:47 +0200

    Changed multiple 2dscale label heights to scale by max height

--- edelphi/src/main/webapp/_scripts/gui/panel/queryblockcontroller.js
+++ edelphi/src/main/webapp/_scripts/gui/panel/queryblockcontroller.js
@@ -448,9 +448,23 @@ Scale1DQueryPageController = Class.create(QueryPageController, {
   }
 });
 
-Multiple2DScaleQueryPageController = Class.create(QueryPageController, {
+var Multiple2DScaleQueryPageController = Class.create(QueryPageController, {
   initialize: function ($super, blockController) {
     $super(blockController);
+    
+    var labelCells = this.getBlockElement().select('td:first-child');
+    
+    var maxHeight = 0;
+    labelCells.each(function (labelCell) {
+      var cellLayout = new Element.Layout(labelCell);
+      maxHeight = Math.max(maxHeight, cellLayout.get('height'));
+    });
+    
+    labelCells.each(function (labelCell) {
+      labelCell.setStyle({
+        'height': maxHeight + 'px'
+      });
+    });
   },
   deinitialize: function ($super) {
     $super();
