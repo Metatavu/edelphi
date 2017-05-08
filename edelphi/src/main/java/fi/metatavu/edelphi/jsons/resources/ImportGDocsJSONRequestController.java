406d6434d1a132bbdf8e735cccc23e0453fe814a
Antti Leppä <antti.leppa@metatavu.fi>
2017-05-08 08:03:11 +0300

    Added support for importing environment level Google documents into json controller

--- edelphi/src/main/java/fi/metatavu/edelphi/jsons/resources/ImportGDocsJSONRequestController.java
+++ edelphi/src/main/java/fi/metatavu/edelphi/jsons/resources/ImportGDocsJSONRequestController.java
@@ -11,23 +11,40 @@ import fi.metatavu.edelphi.EdelfoiStatusCode;
 import fi.metatavu.edelphi.dao.resources.FolderDAO;
 import fi.metatavu.edelphi.dao.resources.GoogleDocumentDAO;
 import fi.metatavu.edelphi.dao.resources.GoogleImageDAO;
+import fi.metatavu.edelphi.dao.resources.ResourceDAO;
 import fi.metatavu.edelphi.dao.users.UserDAO;
-import fi.metatavu.edelphi.domainmodel.actions.DelfoiActionScope;
+import fi.metatavu.edelphi.domainmodel.base.Delfoi;
+import fi.metatavu.edelphi.domainmodel.panels.Panel;
 import fi.metatavu.edelphi.domainmodel.resources.Folder;
+import fi.metatavu.edelphi.domainmodel.resources.Resource;
 import fi.metatavu.edelphi.domainmodel.users.User;
 import fi.metatavu.edelphi.i18n.Messages;
 import fi.metatavu.edelphi.jsons.JSONController;
 import fi.metatavu.edelphi.smvcj.AccessDeniedException;
+import fi.metatavu.edelphi.smvcj.LoginRequiredException;
+import fi.metatavu.edelphi.smvcj.PageNotFoundException;
 import fi.metatavu.edelphi.smvcj.SmvcRuntimeException;
 import fi.metatavu.edelphi.smvcj.controllers.JSONRequestContext;
+import fi.metatavu.edelphi.smvcj.controllers.RequestContext;
 import fi.metatavu.edelphi.utils.GoogleDriveUtils;
 import fi.metatavu.edelphi.utils.ResourceUtils;
 
 public class ImportGDocsJSONRequestController extends JSONController {
 
-  public ImportGDocsJSONRequestController() {
-    super();
-    setAccessAction(DelfoiActionName.MANAGE_PANEL_MATERIALS, DelfoiActionScope.PANEL);
+  @Override
+  public void authorize(RequestContext requestContext) throws LoginRequiredException, AccessDeniedException {
+    ResourceDAO resourceDAO = new ResourceDAO();
+    Long parentFolderId = requestContext.getLong("parentFolderId");
+    if (parentFolderId != null) {
+      Resource resource = resourceDAO.findById(parentFolderId);
+      Panel resourcePanel = ResourceUtils.getResourcePanel(resource);
+      if (resourcePanel != null) {
+        authorizePanel(requestContext, resourcePanel, DelfoiActionName.MANAGE_PANEL_MATERIALS.toString());
+      } else {
+        Delfoi resourceDelfoi = ResourceUtils.getResourceDelfoi(resource);
+        authorizeDelfoi(requestContext, resourceDelfoi, DelfoiActionName.MANAGE_DELFOI_MATERIALS.toString());
+      }
+    }
   }
 
   @Override
@@ -37,12 +54,18 @@ public class ImportGDocsJSONRequestController extends JSONController {
     GoogleDocumentDAO googleDocumentDAO = new GoogleDocumentDAO();
     GoogleImageDAO googleImageDAO = new GoogleImageDAO();
     
+    Locale locale = jsonRequestContext.getRequest().getLocale();
+
     Drive drive = GoogleDriveUtils.getAuthenticatedService(jsonRequestContext);
     if (drive == null) {
       throw new AccessDeniedException(jsonRequestContext.getRequest().getLocale());
     }
     
     Long parentFolderId = jsonRequestContext.getLong("parentFolderId");
+    if (parentFolderId == null) {
+      throw new PageNotFoundException(locale);
+    }
+    
     String[] selectedGDocs = jsonRequestContext.getStrings("selectedgdoc");
     
     try {
@@ -71,7 +94,6 @@ public class ImportGDocsJSONRequestController extends JSONController {
     }
     catch (Exception e) {
       Messages messages = Messages.getInstance();
-      Locale locale = jsonRequestContext.getRequest().getLocale();
       throw new SmvcRuntimeException(EdelfoiStatusCode.GOOGLE_DOCS_FAILURE, messages.getText(locale, "exception.1012.googleDocsFailure"), e);
     }
   }
