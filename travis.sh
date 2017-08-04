fbb0206d76f0018de25d5d814a5bec651fb01b51
Antti Leppä <antti.leppa@metatavu.fi>
2017-08-04 12:18:29 +0300

    Travis

--- travis.sh
+++ travis.sh
@@ -25,6 +25,8 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${
     export PATH=$PATH:$HOME/.local/bin
     export S3_PATH=s3://$AWS_BUCKET/$TRAVIS_REPO_SLUG/$TRAVIS_BUILD_NUMBER
     aws s3 cp itests/target/cargo/configurations/wildfly10x/log $S3_PATH --recursive
+    aws s3 cp itests/target/*.png $S3_PATH
+    aws s3 cp itests/target/*.html $S3_PATH
   fi
   
   exit $TEST_STATUS
