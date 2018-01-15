b8c74491c9f17656f6341d2995e1536e463e9e8c
Antti Leppä <antti.leppa@metatavu.fi>
2018-01-15 10:46:07 +0200

    Bumped tested keycloak into version 3.4.3

--- scripts/keycloak-version.sh
+++ scripts/keycloak-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #export VERSION=`curl -s https://repo1.maven.org/maven2/org/keycloak/keycloak-server-dist/ | sed -e 's/<[^>]*>//g' | grep -i final | cut -d '/' -f1 | tail -n1`
-export VERSION=3.2.1.Final
+export VERSION=3.4.3.Final
 export KEYCLOAK="keycloak-${VERSION}"
