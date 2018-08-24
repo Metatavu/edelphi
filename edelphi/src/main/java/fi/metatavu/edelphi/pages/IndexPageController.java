0d52c2527adc894979519437b76f96d969cdbf2b
Ville Koivukangas <ville.koivukangas12@gmail.com>
2018-08-24 14:08:31 +0300

    spaces

--- edelphi/src/main/java/fi/metatavu/edelphi/pages/IndexPageController.java
+++ edelphi/src/main/java/fi/metatavu/edelphi/pages/IndexPageController.java
@@ -102,28 +102,28 @@ public class IndexPageController extends PageController {
       }
     });
 
-		if (!ignoreBulletinLocale) {
-			TextLanguageDetector langdetect = null;
-			try {
-				langdetect = TextLanguageDetector.getInstance();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		
-			if (langdetect != null) {
-				Iterator<DelfoiBulletin> bulletinIterator = bulletins.iterator();
-		  	
-				while (bulletinIterator.hasNext()) {
-					DelfoiBulletin bulletin = bulletinIterator.next();
-					String text = bulletin.getSummary().toString();
-					String lang = langdetect.getLanguage(text);
-		  	
-					if (lang == null || !lang.equals(locale.getLanguage())) {
-						bulletinIterator.remove();
-					}
-				}
-			}
-		}
+    if (!ignoreBulletinLocale) {
+      TextLanguageDetector langdetect = null;
+      try {
+        langdetect = TextLanguageDetector.getInstance();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    
+      if (langdetect != null) {
+        Iterator<DelfoiBulletin> bulletinIterator = bulletins.iterator();
+        	
+        while (bulletinIterator.hasNext()) {
+          DelfoiBulletin bulletin = bulletinIterator.next();
+          String text = bulletin.getSummary().toString();
+          String lang = langdetect.getLanguage(text);
+        
+          if (lang == null || !lang.equals(locale.getLanguage())) {
+            bulletinIterator.remove();
+          }
+        }
+      }
+    }
         
 	  Long authSourceId = AuthUtils.getAuthSource("Keycloak").getId();
 	  String pageContents = getIndexPageContents(delfoi, locale);
