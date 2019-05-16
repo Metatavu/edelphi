ece33f0b96a5418d3d9ec48948b74bcc45bcbf72
Antti Leppä <antti.leppa@metatavu.fi>
2019-05-16 06:59:42 +0300

    Generated source

--- rest-spec/jaxrs-spec-generated/src/main/java/fi/metatavu/edelphi/rest/api/PanelsApi.java
+++ rest-spec/jaxrs-spec-generated/src/main/java/fi/metatavu/edelphi/rest/api/PanelsApi.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 
 @Path("/panels")
 @Api(description = "the panels API")
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-05-16T06:58:51.539+03:00[Europe/Helsinki]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-05-16T06:59:36.665+03:00[Europe/Helsinki]")
 public interface PanelsApi {
 
     @POST
