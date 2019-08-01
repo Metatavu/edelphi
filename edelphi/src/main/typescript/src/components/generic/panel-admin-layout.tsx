a3337b118b2a6eda0cad9a19b0eae674fa6a3d06
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-01 08:43:37 +0300

    Added locale change links into react based admin header

--- edelphi/src/main/typescript/src/components/generic/panel-admin-layout.tsx
+++ edelphi/src/main/typescript/src/components/generic/panel-admin-layout.tsx
@@ -74,14 +74,16 @@ class PanelAdminLayout extends React.Component<Props, State> {
         <Container>
           <Grid>
             <Grid.Row>
-              <Grid.Column>
+              <Grid.Column width={ 6 }>
                 <h1 className="header-title">
                   <a className="root-link" href="/">eDelphi.org</a>
                   <a className="panel-link" href={ "/" + this.props.panel.urlName }>{ this.props.panel.name }</a>
                 </h1>
               </Grid.Column>
-              <Grid.Column>
-      
+              <Grid.Column width={ 6 } textAlign="center">
+                { this.renderLocaleChange() }
+              </Grid.Column>
+              <Grid.Column width={ 6 }>
               </Grid.Column>
             </Grid.Row>
 
@@ -107,6 +109,64 @@ class PanelAdminLayout extends React.Component<Props, State> {
     return (
       <a onClick={ this.props.onBackLinkClick } className="header-back-link"> { strings.generic.back } </a>
     );
+  }
+
+  /**
+   * Renders locale change links
+   */
+  private renderLocaleChange = () => {
+    const selectedLanguage = strings.getLanguage();
+
+    return (
+      <div>
+        <a className={ selectedLanguage == "fi" ? "header-locale-link header-locale-link-selected" : "header-locale-link"} href="#" onClick={ this.onLocaleChangeFiClick }>Suomeksi</a>
+        <a className={ selectedLanguage == "en" ? "header-locale-link header-locale-link-selected" : "header-locale-link"} href="#" onClick={ this.onLocaleChangeEnClick }>In English</a>
+      </div>
+    );
+  }
+
+  /**
+   * Changes legacy UI locale
+   * 
+   * @param locale locale
+   */
+  private changeLegacyLocale = async (locale: string) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (3650*24*60*60*1000));
+    const expires = "; expires=" + date.toUTCString();
+    document.cookie = "eDelphiLocale=" + locale + expires + "; path=/";
+    await (await fetch(`/locale/setlocale.json?locale=${locale}`)).json();
+  }
+
+  /**
+   * Changes locale
+   * 
+   * @param locale locale
+   */
+  private changeLocale = async (locale: string) => {
+    await this.changeLegacyLocale(locale);
+    // TODO: locale to redux
+    window.location.reload(true);
+  }
+
+  /**
+   * Event handler for locale fi link click
+   * 
+   * @param event event
+   */
+  private onLocaleChangeFiClick = async (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    await this.changeLocale("fi");
+  }
+
+  /**
+   * Event handler for locale en link click
+   * 
+   * @param event event
+   */
+  private onLocaleChangeEnClick = async (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    await this.changeLocale("en");
   }
 }
 
