e3765d60d1b5f9f11b77158b45ff49d5ad16d095
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-06 10:40:30 +0300

    Generated source

--- rest-spec/jaxrs-spec-generated/src/main/java/fi/metatavu/edelphi/rest/api/UsersApi.java
+++ rest-spec/jaxrs-spec-generated/src/main/java/fi/metatavu/edelphi/rest/api/UsersApi.java
@@ -13,19 +13,19 @@ import java.util.List;
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 
-@Path("/reportRequests")
-@Api(description = "the reportRequests API")
+@Path("/users")
+@Api(description = "the users API")
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJAXRSSpecServerCodegen", date = "2019-08-06T10:40:24.181+03:00[Europe/Helsinki]")
-public interface ReportRequestsApi {
+public interface UsersApi {
 
-    @POST
-    @Consumes({ "application/json" })
+    @GET
+    @Path("/{userId}")
     @Produces({ "application/json" })
-    @ApiOperation(value = "Creates a report request", notes = "Creates a request to generate a report", authorizations = {
-        @Authorization(value = "bearer")    }, tags={ "Reports" })
+    @ApiOperation(value = "Find user", notes = "Finds an user by id", authorizations = {
+        @Authorization(value = "bearer")    }, tags={ "Users" })
     @ApiResponses(value = { 
-        @ApiResponse(code = 202, message = "Report request has been accepted", response = Void.class),
+        @ApiResponse(code = 200, message = "Found user", response = User.class),
         @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = ErrorResponse.class),
         @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = ErrorResponse.class),
         @ApiResponse(code = 500, message = "Internal server error", response = ErrorResponse.class) })
-    Response createReportRequest(@Valid ReportRequest body);}
+    Response findUser(@PathParam("userId")  UUID userId);}
