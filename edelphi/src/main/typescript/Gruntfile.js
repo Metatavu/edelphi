730aa4d0646c43de5d6be5b045ba20e0a5de11e6
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-13 19:18:10 +0300

    Added better compatibility to npx runner

--- edelphi/src/main/typescript/Gruntfile.js
+++ edelphi/src/main/typescript/Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     "shell": {
       "webpack": {
-        "command": "npx webpack",
+        "command": "node node/node_modules/npm/bin/npx-cli.js webpack",
         "options": {
           "execOptions": {
             cwd: "."
