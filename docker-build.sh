fe7d3d7fdd0c2f939a72d422abddbbdb3610af25
Antti Leppä <antti.leppa@metatavu.fi>
2019-11-13 07:31:53 +0200

    Moved legacy docker into edelphi folder

--- docker-build.sh
+++
@@ -1,5 +0,0 @@
-mvn clean package
-VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)')
-docker build -t edelphi .
-docker tag $(docker images -q edelphi) metatavu/edelphi:$VERSION
-docker push metatavu/edelphi
