9d5496226b33a7bd09b621a46b5276b8eb2d6f10
Antti Leppä <antti.leppa@metatavu.fi>
2021-10-18 13:02:39 +0300

    Removed deprecated spec generation files

--- rest-spec/typescript-client-generated/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-var api_1 = require("./api/api");
-__export(require("./model/models"));
-exports.default = api_1.default;
