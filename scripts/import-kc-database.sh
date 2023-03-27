3fc7cc16b5e2f1b89b35ab6e48fcde59f20199a5
Antti Leppä <antti.leppa@metatavu.fi>
2023-03-27 08:23:44 +0300

    Fixed issues with running docker-compose with arm devices

--- scripts/import-kc-database.sh
+++ scripts/import-kc-database.sh
@@ -12,6 +12,6 @@ docker cp ../db_dumps/keycloak.sql $CONTAINER_ID:/tmp/kc.sql
 echo "Copied database dump for Keycloak"
 
 echo "Creating databases with dump data..."
-docker exec $CONTAINER_ID  mysql -uroot -proot -e 'DROP DATABASE IF EXISTS `keycloak`; CREATE DATABASE `keycloak` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */; USE keycloak; source /tmp/kc.sql; commit;'
+docker exec $CONTAINER_ID  mysql -uroot -proot -e 'DROP DATABASE IF EXISTS `keycloak`; CREATE DATABASE `keycloak` /*!40100 DEFAULT CHARACTER SET utf8 */; USE keycloak; source /tmp/kc.sql; commit;'
 
 docker-compose down
