b8264ce4ac044ad216c67de3aa9a6029817bc465
Antti Leppä <antti.leppa@metatavu.fi>
2024-05-17 08:49:45 +0300

    Added support for versioning javascript files

---
+++ edelphi/src/main/resources/fi/metatavu/edelphi/Version.java
@@ -0,0 +1,13 @@
+package fi.metatavu.edelphi;
+
+/**
+ * Generated version information
+ */
+public class Version {
+
+    /**
+     * Current project version
+     */
+    public static final String VERSION = "${version}";
+
+}
