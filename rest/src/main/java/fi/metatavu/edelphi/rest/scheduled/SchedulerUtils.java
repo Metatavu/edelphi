943c3ecdb8edefa8402f107ee6adffe7da5af43b
Simeon Platonov <simeon.platonov@gmail.com>
2025-04-15 16:03:29 +0300

    Remove redundant

--- rest/src/main/java/fi/metatavu/edelphi/rest/scheduled/SchedulerUtils.java
+++ rest/src/main/java/fi/metatavu/edelphi/rest/scheduled/SchedulerUtils.java
@@ -10,14 +10,6 @@ import java.time.ZonedDateTime;
  *
  */
 public class SchedulerUtils {
-  static boolean deletionSchedulersActive() {
-    int schedulerStartHour = 0; // 00-01 (In FINNISH time 21 in UTC) archiving panels 01-02 deleting panels 02-03 archiving users 03-06 deleting users, panels every 15 minute, user archiving 1 minute, user deletion every 5 minute
-    int schedulerEndHour = 6;
-    int currentHour = ZonedDateTime.now(ZoneOffset.UTC).getHour();
-
-    return currentHour >= schedulerStartHour || currentHour < schedulerEndHour;
-  }
-
   static boolean panelArchivingScheduleActive() {
     int schedulerStartHour = 21;
     int currentHour = ZonedDateTime.now(ZoneOffset.UTC).getHour();
